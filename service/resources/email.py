59665b66a75968292a8c07d6391184637cddac2a
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2020-02-21 14:32:51 -0800

    Sendgrid API feature implementation, sample email data file

--- service/resources/email.py
+++ service/resources/email.py
@@ -1,44 +1,109 @@
 from sendgrid import SendGridAPIClient
+from python_http_client.exceptions import HTTPError
 from sendgrid.helpers.mail import (
-    Mail, From, To, Cc, Bcc, Subject, Header, SendAt, Content, MimeType, ReplyTo, MailSettings, FooterSettings, FooterText,
-    FooterHtml)
+    Mail, From, To, Cc, Bcc, Subject, SendAt, MimeType, TemplateId,
+    Asm, GroupId, GroupsToDisplay, ReplyTo, BatchId)
 import json
 import falcon
 from .helpers import helpers
 from .hooks import validate_access
+import sys, traceback, logging
 
 @falcon.before(validate_access)
 class EmailService():
 
     def on_post(self, req, resp):
-        local_helper = helpers.HelperService()
         data = json.loads(req.stream.read())
 
-        to_emails = [
-            data['dynamic_template_data']['from']
-        ]
-        message = Mail(
-            from_email=data['dynamic_template_data']['from'],
-            to_emails=to_emails
-            )
+        if 'personalizations' in data.keys() and data['personalizations'] is not None:
+            for personalization in data['personalizations']:
+                """ required parameters """
+                data['to'] = personalization['to']
+                """ optional """
+                if 'subject' in personalization.keys() and  personalization['subject'] is not None:
+                    data['subject'] = personalization['subject']
+                if 'bcc' in personalization.keys() and   personalization['bcc'] is not None:
+                    data['bcc'] = personalization['bcc']
+                if 'cc' in personalization.keys() and   personalization['cc'] is not None:
+                    data['cc'] = personalization['cc']
+                if 'send_at' in personalization.keys() and   personalization['send_at'] is not None:
+                    data['send_at'] = personalization['send_at']
+                if 'headers' in personalization.keys() and  personalization['headers'] is not None:
+                    data['headers'] = personalization['headers']
+                if 'custom_args' in personalization.keys() and   personalization['custom_args'] is not None:
+                    data['custom_args'] = personalization['custom_args']
+                self.sendEmail(data, resp)
+        else:
+            self.sendEmail(data, resp)
 
-        message.content = Content(MimeType.text, data['text'])
-        message.content = Content(MimeType.html, data['html'])
+    def sendEmail(self, data, resp):
+        local_helper = helpers.HelperService()
+        """ Construct required outgoing email parameters """
+        message = Mail()
 
-        message.dynamic_template_data = {
-            'subject': data['dynamic_template_data']['subject'],
-            'name': data['dynamic_template_data']['name'],
-            'city': data['dynamic_template_data']['city'],
-        }
-        message.template_id = data['template_id']
+        """ One line settings """
+        message.from_email = From(data['from']['email'], data['from']['name'])
+        message.subject = Subject(data['subject'])
 
-        if data['files'] is not None:
-            for afile in data['files']:
-                message.add_attachment(local_helper.getAttachments(afile))
+        """ Set email address to send to """
+        if 'to' in data.keys() and data['to'] is not None:
+           message.to = local_helper.getEmails(data['to'], 'to')
+
+        """ Set CC """
+        if 'cc' in data.keys() and data['cc'] is not None:
+            message.cc = local_helper.getEmails(data['cc'], 'cc')
+        """ Set Bcc """
+        if 'bcc' in data.keys() and data['bcc'] is not None:
+            message.bcc = local_helper.getEmails(data['bcc'], 'bcc')
+
+        """ If content is set, use as email content, otherwise template must be used """
+        if 'content' in data.keys() and data['content'] is not None:
+            message.content = local_helper.getContent(data['content'])
+
+        if 'send_at' in data.keys() and data['send_at'] is not None:
+            message.send_at = data['send_at']
+        if 'asm' in data.keys() and data['asm'] is not None:
+            message.asm = Asm(GroupId(data['asm']['group_id']), GroupsToDisplay(data['asm']['groups_to_display']))
+        if 'batch_id' in data.keys() and data['batch_id'] is not None:
+            message.batch_id = BatchId(data['batch_id'])
+
+        """ If template id is specified, set dynamic data for the template """
+        if 'template_id' in data.keys() and data['template_id'] != "":
+            message.template_id = TemplateId(data['template_id'])
+            template_data = {}
+            template_data.update(data['dynamic_template_data'])
+            message.dynamic_template_data = template_data
+
+        """ Additional Sendgrid API features """
+        #if 'attachments' in data.keys() and data['attachments'] is not None:
+            #message.attachment = local_helper.getAttachments(data['attachments'])
+
+        if 'tracking_settings' in data.keys() and data['tracking_settings'] is not None:
+            message.tracking_settings = local_helper.getEmailTrackings(data['tracking_settings'])
+
+        if 'custom_args' in data.keys() and data['custom_args'] is not None:
+            message.custom_arg = local_helper.getCustomArgs(data['custom_args'])
+
+        if 'email_settings' in data.keys() and data['mail_settings'] is not None:
+            message.mail_settings = local_helper.getMailSettings(data['mail_settings'])
+
+        if 'sections' in data.keys() and data['sections'] is not None:
+            message.section = local_helper.getSections(data['sections'])
+
+        if 'headers' in data.keys() and data['headers'] is not None:
+            message.header = local_helper.getHeaders(data['headers'])
+
+        if 'categories' in data.keys() and data['categories'] is not None:
+            message.category = local_helper.getCategory(data['categories'])
+
         try:
-            sg = SendGridAPIClient(data['SENDGRID_API_KEY'])
-            response = sg.send(message)
+            logging.warning(message.get())
+            sendgrid_client = SendGridAPIClient(data['SENDGRID_API_KEY'])
+            response = sendgrid_client.send(message)
             resp.body = response.body
             resp.status = falcon.HTTP_200
-        except Exception as e:
-            resp.body = json.dumps(str(e))
+        except HTTPError as error:
+            print(error.to_dict)
+        except Exception as error:
+            resp.body = json.dumps(str(error))
+
--- service/resources/helpers/helpers.py
+++ service/resources/helpers/helpers.py
@@ -1,30 +1,136 @@
 import base64
+import logging
+from sendgrid.helpers.mail import (
+    To, Cc, Bcc, Substitution, Header,
+    CustomArg, Content, MimeType, Attachment, FileName,
+    FileContent, FileType, Disposition, ContentId,
+    Section, Category,
+    IpPoolName, MailSettings, BccSettings, BccSettingsEmail,
+    BypassListManagement, FooterSettings, FooterText,
+    FooterHtml, SandBoxMode, SpamCheck, SpamThreshold, SpamUrl,
+    TrackingSettings, ClickTracking, SubscriptionTracking,
+    SubscriptionText, SubscriptionHtml, SubscriptionSubstitutionTag,
+    OpenTracking, OpenTrackingSubstitutionTag, Ganalytics,
+    UtmSource, UtmMedium, UtmTerm, UtmContent, UtmCampaign)
 
 class HelperService():
-    # Email attachment
-    def getAttachments(self, thisfile):
-        """ This function sets up email attachments
-
-        """
-        from sendgrid.helpers.mail import (
-            Mail, Attachment, FileContent, FileName,
-            FileType, Disposition, ContentId)
+    def getAttachments(self, attachments):
+        """ This function sets up email attachments """
         try:
             # Python 3
             import urllib.request as urllib
         except ImportError:
             # Python 2
             import urllib2 as urllib
+        attachment_list = []
 
-        file_path = thisfile['path']
-        data = urllib.urlopen(file_path).read()
+        for attachment in attachments:
+            if 'path' in attachment.keys() and attachment['path'] is not None and attachment['path'] != "":
+                file_path = attachment['path']
+                data = urllib.urlopen(file_path).read()
+                encoded = base64.b64encode(data).decode()
+            else:
+                encoded = base64.b64encode(bytes(attachment['content'], 'utf-8')).decode()
 
-        encoded = base64.b64encode(data).decode()
-        attachment = Attachment()
-        attachment.file_content = FileContent(encoded)
-        attachment.file_type = FileType(thisfile['mimetype'])
-        attachment.file_name = FileName(thisfile['name'])
-        attachment.disposition = Disposition('attachment')
-        attachment.content_id = ContentId('Unique file ID')
+            attachment_list.append(Attachment(
+                FileContent(encoded),
+                FileName(attachment['filename']),
+                FileType(attachment['type'])
+            ))
 
-        return attachment
+        return attachment_list
+
+    def getEmailTrackings(self, settings):
+        tracking_settings = TrackingSettings()
+        if 'click_tracking' in settings.keys() and settings['click_tracking'] is not None:
+            tracking_settings.click_tracking = ClickTracking(settings['click_tracking']['enable'], settings['click_tracking']['enable_text'])
+        if 'open_tracking' in settings.keys() and settings['open_tracking'] is not None:
+            tracking_settings.open_tracking = OpenTracking(
+                settings['open_tracking']['enable'],
+                OpenTrackingSubstitutionTag(settings['open_tracking']['substitution_tag'])
+            )
+        if 'subscription_tracking' in settings.keys() and settings['subscription_tracking'] is not None:
+            tracking_settings.subscription_tracking = SubscriptionTracking(
+                settings['subscription_tracking']['enable'],
+                SubscriptionText(settings['subscription_tracking']['text']),
+                SubscriptionHtml(settings['subscription_tracking']['html']),
+                SubscriptionSubstitutionTag(settings['subscription_tracking']['substitution_tag']))
+        if 'ganalytics' in settings.keys() and settings['ganalytics'] is not None:
+            tracking_settings.ganalytics = Ganalytics(
+                settings['ganalytics']['enable'],
+                UtmSource(settings['ganalytics']['utm_source']),
+                UtmMedium(settings['ganalytics']['utm_medium']),
+                UtmTerm(settings['ganalytics']['utm_term']),
+                UtmContent(settings['ganalytics']['utm_content']),
+                UtmCampaign(settings['ganalytics']['utm_campaign'])
+            )
+        return tracking_settings
+
+    def getCustomArgs(self, custom_args):
+        custom_arg_list = []
+        for custom_arg_key, custom_arg_value in custom_args.items():
+            custom_arg_list.append(CustomArg(custom_arg_key, custom_arg_value))
+        return custom_arg_list
+
+    def getMailSettings(self, settings):
+        mail_settings = MailSettings()
+        if 'bcc' in settings.keys() and settings['bcc'] is not None:
+            mail_settings.bcc_settings = BccSettings(
+                settings['bcc']['enable'],
+                BccSettingsEmail(settings['bcc']['email'])
+            )
+        if 'bypass_list_management' in settings.keys() and settings['bypass_list_management'] is not None:
+            mail_settings.bypass_list_management = BypassListManagement(
+                settings['bypass_list_management']['enable'])
+        if 'footer' in settings.keys() and settings['footer'] is not None:
+            mail_settings.footer_settings = FooterSettings(
+                settings['footer']['enable'],
+                FooterText(settings['footer']['text']),
+                FooterHtml(settings['footer']['html'])
+            )
+        if 'sandbox_mode' in settings.keys() and settings['sandbox_mode'] is not None:
+            mail_settings.sandbox_mode = SandBoxMode(settings['sandbox_mode']['enable'])
+        if 'spam_check' in settings.keys() and settings['spam_check'] is not None:
+            mail_settings.spam_check = SpamCheck(
+                settings['spam_check']['enable'],
+                SpamThreshold(settings['spam_check']['threshold']),
+                SpamUrl(settings['spam_check']['post_to_url'])
+            )
+        return mail_settings
+
+    def getSections(self, sections):
+        email_sections = []
+        for section_key, section_value in sections.items():
+            email_sections.append(Section(section_key, section_value))
+        return email_sections
+
+    def getHeaders(self, headers):
+        email_headers = []
+        for header_key, header_value in headers.items():
+            email_headers.append(Header(header_key, header_value))
+        return email_headers
+
+    def getCategory(self, categories):
+        email_categories = []
+        for category in categories:
+            email_categories.append(Category(category))
+        return email_categories
+
+    def getEmails(self, emails, email_type):
+        email_list = []
+        counter = 0
+        for email in emails:
+            if email_type == 'to':
+                email_list.append(To(email['email'], email['name'], p=counter))
+            elif email_type == 'cc':
+                email_list.append(Cc(email['email'], email['name'], p=counter))
+            elif email_type == 'bcc':
+                email_list.append(Bcc(email['email'], email['name'], p=counter))
+            counter += 1
+        return email_list
+
+    def getContent(self, contents):
+        content_list = []
+        for content in contents:
+            content_list.append(Content(content['type'], content['value']))
+        return content_list
