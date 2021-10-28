2bff80d025ed7169aa5f13e8621ef3824db58b31
Josh Chou <626661+josh-chou@users.noreply.github.com>
2021-10-27 17:39:07 -0700

    surface send_grid errors in log and don't retry

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,12 +1,13 @@
 """ Tests for email module """
 from unittest.mock import patch
 import pytest
+from python_http_client.exceptions import HTTPError
 from service.resources.db import HistoryModel
 from tests import mocks
 from tests.fixtures import db_session # pylint: disable=unused-import
 from tasks import generate_template_content, utc_to_pacific, multiselect_dict_to_list, uploads_to_list, send_email, ERR_MSG_MAX_RETRIES
 
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name,unused-argument
 @patch('urllib.request.urlopen')
 def test_generate_template_content(mock_urlopen):
     """ test generate_template_content function """
@@ -83,19 +84,63 @@ def test_send_email_task(mock_sendgrid_client, mock_urlopen, db_session):
 
     db_session.delete(history)
 
-    # error when calling sendgrid_client.send()
-    mock_sendgrid_client.return_value.send.side_effect = Exception("sendgrid error")
+@patch('urllib.request.urlopen')
+@patch('sendgrid.SendGridAPIClient')
+def test_send_email_task_generic_error(mock_sendgrid_client, mock_urlopen, db_session):
+    """ test generic error when calling sendgrid_client.send() """
+    mock_sendgrid_client.return_value.send.side_effect = Exception("generic send error")
+    mock_urlopen.return_value.__enter__.return_value.read.side_effect = [mocks.EMAIL_HTML, b"fake_data", b"fake_data"]
 
-    history2 = HistoryModel(request=params)
-    db_session.add(history2)
+    params = mocks.EMAIL_POST.copy()
+    del params['content']
+    params['template'] = mocks.TEMPLATE_PARAMS
+
+    history = HistoryModel(request=params)
+    db_session.add(history)
     db_session.commit()
 
-    send_email.s(history2.id).apply()
+    send_email.s(history.id).apply()
 
-    db_session.refresh(history2)
-    assert history2.result == ERR_MSG_MAX_RETRIES
-    assert len(history.email_content) == 2
-    assert history2.processed_timestamp is None
+    db_session.refresh(history)
+    assert history.result == ERR_MSG_MAX_RETRIES
+    assert history.email_content is None
+    assert history.processed_timestamp is None
 
-    db_session.delete(history2)
+    db_session.delete(history)
     db_session.commit()
+
+@patch('urllib.request.urlopen')
+@patch('sendgrid.SendGridAPIClient')
+def test_send_email_task_sendgrid_error(mock_sendgrid_client, mock_urlopen, db_session):
+    """ test sendgrid error when calling sendgrid_client.send() """
+    print("test_send_email_task_sendgrid_error")
+    mock_sendgrid_client.return_value.send.side_effect = raise_http_error
+    mock_urlopen.return_value.__enter__.return_value.read.side_effect = [mocks.EMAIL_HTML, b"fake_data", b"fake_data"]
+
+    params = mocks.EMAIL_POST.copy()
+    del params['content']
+    params['template'] = mocks.TEMPLATE_PARAMS
+
+    history = HistoryModel(request=params)
+    db_session.add(history)
+    db_session.commit()
+
+    send_email.s(history.id).apply()
+
+    db_session.refresh(history)
+    assert history.result == ERR_MSG_MAX_RETRIES
+    assert history.email_content is None
+    assert history.processed_timestamp is None
+
+    db_session.delete(history)
+    db_session.commit()
+
+def raise_http_error(arg):
+    """ raise HTTPError """
+    print("raise HTTPError")
+    raise HTTPError(
+        400,
+        'sendgrid error reason',
+        'sendgrid error body',
+        None
+    )
