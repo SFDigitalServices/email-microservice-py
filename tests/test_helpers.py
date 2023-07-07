a8f962bfab016ac6e579ef4c1c8c5cfef7f4c20e
Derek Chinn <derek.chinn@sfgov.org>
2023-07-07 13:25:20 -0700

    Appeasing the linting gods

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,9 +1,16 @@
+"""
+Testing for the HelperService module.
+"""
+
 import pytest
 from sendgrid.helpers.mail import To, Cc, Bcc
 
 from service.resources.helpers.helpers import HelperService
 
 class TestGetEmails():
+    """
+    Grouping of test related to get_emails method
+    """
     @pytest.mark.parametrize('email_type', ['cc', 'bcc'])
     @pytest.mark.parametrize('test_input,expected', [
         ([{}], 0),
@@ -13,6 +20,10 @@ class TestGetEmails():
         ([{'name': 'Name'}, {'email': 'test@test.com', 'name': 'Name'}], 1),
     ])
     def test_optional_email_types(self, email_type, test_input, expected):
+        """
+        Ensure that "invalid" entries are removed and not added to the
+        final return emails addresses to send to.
+        """
         ret = HelperService.get_emails(test_input, email_type)
         assert len(ret) == expected
 
@@ -22,6 +33,10 @@ class TestGetEmails():
         ([{'email': 'test@test.com', 'name': 'Name'}], 'Name')
     ])
     def test_name_field_is_optional(self, email_type, test_input, expected):
+        """
+        Ensure that the "name" is an optional field and handle it appropriately
+        when no name is passed.
+        """
         ret = HelperService.get_emails(test_input, email_type)
         assert ret[0].name == expected
 
@@ -31,5 +46,8 @@ class TestGetEmails():
         ('bcc', [{'email': 'test@test.com'}], Bcc)
     ])
     def test_appropriate_return_type(self, email_type, test_input, expected):
+        """
+        Ensure that we get back the appropriate sengrid mail type back.
+        """
         ret = HelperService.get_emails(test_input, email_type)
         assert isinstance(ret[0], expected)
